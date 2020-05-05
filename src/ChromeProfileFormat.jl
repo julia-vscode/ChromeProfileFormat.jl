b0794dabe22b7cf9b816a04a171809c696154ce5
Valentin Churavy <v.churavy@gmail.com>
2020-05-05 17:36:37 -0400

    Decode Julia profile

--- src/ChromeProfileFormat.jl
+++ src/ChromeProfileFormat.jl
@@ -2,123 +2,284 @@ module ChromeProfileFormat
 
 import JSON, Profile
 
-function save_cpuprofile(filename, data, lidict)
-    start_time = 386524903102 # TODO Come up with something better
-    time_delta = 500
+mutable struct CallFrame
+    functionName::String
+    scriptId::String # unique script identifier
+    url::String
+    lineNumber::Int64
+    columnNumber::Int64
+end
 
-    script_ids = Dict{Symbol,Int}()
-    next_script_id = 0
+struct PositionTickInfo
+    line::Int64
+    ticks::Int64
+end
 
-    node_ids = Dict{UInt,Int}()
-    next_node_id = 1
+"""
+    ProfileNode
 
-    for i in unique(data)
-        node_ids[i] = next_node_id
-        next_node_id += 1
-    end
+A `ProfileNode` represents a method in the call-graph. Each child in `children`
+is a callee and the `positionTicks` represents the location.
+"""
+mutable struct ProfileNode
+    id::Int64
+    callFrame::CallFrame
+    hitCount::Int64 
+    children::Vector{Int64}
+    positionTicks::Vector{PositionTickInfo}
+end
 
-    open(filename, "w") do f
-        print(f, "{")
-
-        JSON.print(f, "startTime")
-        print(f, ":")
-        
-        JSON.print(f, start_time)
-
-        print(f, ",")
-
-        JSON.print(f, "endTime")
-        print(f, ":")
-        JSON.print(f, start_time + length(data) * time_delta)        
-
-        print(f, ",")
-
-
-        JSON.print(f, "nodes")
-        print(f, ":[")
-        
-        for (i, (k, v)) in ((i, (k, lidict[k])) for (i,k) in enumerate(unique(data)))
-            if i>1
-                print(f, ",")
-            end
-
-            print(f, "{")
-
-            JSON.print(f, "id")
-            print(f, ":")
-            JSON.print(f, node_ids[k])
-
-            print(f, ",")
-
-            JSON.print(f, "callFrame")
-            print(f, ":")
-
-            print(f, "{")
-
-            JSON.print(f, "functionName")
-            print(f, ":")
-            JSON.print(f, v.func)
-
-            print(f, ",")
-
-            script_id = get(script_ids, v.file, -1)
-
-            if script_id == -1
-                script_id = next_script_id
-                script_ids[v.file] = script_id
-                next_script_id += 1
-            end
-
-            JSON.print(f, "scriptId")
-            print(f, ":")
-            JSON.print(f, string(script_id))
-
-            print(f, ",")
-
-            JSON.print(f, "url")
-            print(f, ":")
-            JSON.print(f, v.file)
-
-            print(f, ",")
-
-            # TODO I think this is wrong, because this probably now refers to a line in the function, not the definition of the function?
-            JSON.print(f, "lineNumber")
-            print(f, ":")
-            JSON.print(f, v.line>0 ? v.line-1 : 0)
-
-            print(f, ",")
-
-            # TODO I think this is wrong, because this probably now refers to a line in the function, not the definition of the function?
-            JSON.print(f, "columnNumber")
-            print(f, ":")
-            JSON.print(f, 0)
-
-            print(f, "}")
-
-            print(f, ",")
-
-            JSON.print(f, "children")
-            print(f, ":")
-            JSON.print(f, Int[])
-
-            print(f, "}")
-        end
-        print(f, "]")
-
-        print(f, ",")
-
-        JSON.print(f, "samples")
-        print(f, ":")
-        JSON.print(f, [node_ids[i] for i in data])
-
-        print(f, ",")
-
-        JSON.print(f, "timeDeltas")
-        print(f, ":")
-        JSON.print(f, fill(time_delta, length(data)))
-
-        print(f, "}")
+function enter!(node::ProfileNode, line)
+    idx = findfirst(tick->tick.line == line, node.positionTicks)
+    if idx === nothing
+        push!(node.positionTicks, PositionTickInfo(line, 1))
+    else
+        old = node.positionTicks[idx]
+        node.positionTicks[idx] = PositionTickInfo(line, old.ticks + 1)
     end
 end
 
+struct CPUProfile
+    nodes::Vector{ProfileNode}
+    startTime::Int64
+    endTime::Int64
+    samples::Vector{Int64}
+    timeDeltas::Vector{Int64}
+end
+
+"""
+    CPUProfile(data, period)
+
+Fetches the collected `Profile` data.
+
+# Arguments:
+- `data::Vector{UInt}`: The data provided by `Profile.fetch` [optional].
+- `period::UInt64`: The sampling period in nanoseconds [optional].
+"""
+
+function CPUProfile(data::Union{Nothing, Vector{UInt}} = nothing,
+                    period::Union{Nothing, UInt64} = nothing; from_c = false)
+    if data === nothing
+        data = copy(Profile.fetch())
+    end
+    lookup = Profile.getdict(data)
+    if period === nothing
+        period = ccall(:jl_profile_delay_nsec, UInt64, ())
+    end
+
+    period = period ÷ 1000 # ns to ms
+
+    methods = Dict{Tuple{String, Int64}, CallFrame}()
+    function get_callframe!(url, start_line, name, file)
+        get!(methods, (url, start_line)) do
+            CallFrame(name, file, url, start_line, 0)
+        end
+    end
+
+    nodes = ProfileNode[]
+    function insert_node!(callframe)
+        node = ProfileNode(length(nodes) + 1, callframe, 0, Int64[], [])
+        push!(nodes, node)
+        return node
+    end
+
+    samples    = Int64[] #  Ids of samples leaf.
+    toplevel   = Set{Int64}() #  Ids of samples top nodes.
+    timeDeltas = Int64[] # Time intervals between adjacent samples in microseconds. The first delta is relative to the profile startTime.
+
+    # start decoding backtraces
+    lastwaszero = true
+    current = nothing
+
+    # data is in bottom-up order therefore reverse it
+    for ip in Iterators.reverse(data)
+        # ip == 0x0 is the sentinel value for finishing a backtrace, therefore finising a sample
+        if ip == 0
+            # Avoid creating empty samples
+            if lastwaszero
+                @assert current === nothing
+                continue
+            end
+            lastwaszero = true
+
+            if current !== nothing
+                push!(samples, current.id)
+                push!(timeDeltas, period)
+            end
+
+            current = nothing
+            continue
+        end
+        lastwaszero = false
+
+        # A backtrace consists of a set of IP (Instruction Pointers), each IP points
+        # a single line of code and `litrace` has the necessary information to decode
+        # that IP to a specific frame (or set of frames, if inlining occured).
+
+        # decode the inlining frames
+        for frame in Iterators.reverse(lookup[ip])
+            # ip 0 is reserved
+            frame.pointer == 0 && continue
+            frame.from_c && !from_c && continue
+
+            # A `frame` is an entry in a backtrace. A `ProfileNode` is a method in the call-graph
+            # which can be hit by multiple frames. So first we need to go from frame to method.
+            # As keys we will use `url` + `start_line`.
+            if frame.linfo === nothing # inlined frame
+                file = string(frame.file)
+                name = string(frame.func)
+                start_line = convert(Int64, frame.line) # TODO: Get start_line properly
+            else
+                if frame.linfo isa Core.MethodInstance
+                    linfo = frame.linfo::Core.MethodInstance
+                elseif frame.linfo isa Core.CodeInfo
+                    # TODO: stackframes.jl describes this as a top-level frame
+                    linfo = frame.linfo.parent::Core.MethodInstance
+                end
+                meth       = linfo.def
+                file       = string(meth.file)
+                name       = string(meth.module, ".", meth.name)
+                start_line = convert(Int64, meth.line)
+            end
+            # TODO: Deal with unkown or unresolved functions
+            url = Base.find_source_file(file)
+            if url === nothing
+                url = file
+            end
+
+            # We use callframe as a identifier
+            callframe = get_callframe!(url, start_line, name, file)
+
+            node = nothing
+            if current === nothing # top-level
+                # have we seen this callframe before?
+                for id in toplevel
+                    if nodes[id].callFrame === callframe
+                        node = nodes[id]
+                    end
+                end
+                if node === nothing
+                    node = insert_node!(callframe)
+                end
+                node.hitCount += 1
+                push!(toplevel, node.id)
+
+            else
+                # is this callframe present in our parent?
+                for id in current.children
+                    if nodes[id].callFrame === callframe
+                        node = nodes[id]
+                    end
+                end
+                if node === nothing
+                    node = insert_node!(callframe)
+                    push!(current.children, node.id)
+                end
+            end
+            # mark the new line as hit
+            enter!(node, frame.line)
+            current = node
+        end
+    end
+
+    if current !== nothing
+        push!(samples, current.id)
+        push!(timeDeltas, period)
+    end
+
+    start_time = 0
+    timeDeltas[1] = 0
+    @assert length(timeDeltas) == length(samples)
+    return CPUProfile(nodes, start_time, start_time + sum(timeDeltas), samples, timeDeltas) 
+end
+
+function save_cpuprofile(filename, data::Union{Nothing, Vector{UInt}} = nothing,
+                         period::Union{Nothing, UInt64} = nothing; kwargs...)
+
+    profile = CPUProfile(data, period; kwargs...)
+    open(filename, "w") do io
+        write(io, '{')
+        JSON.print(io, "nodes")
+        write(io, ':')
+        write(io, '[')
+        for (i, node) in enumerate(profile.nodes)
+            write(io, '{')
+            JSON.print(io, "id")
+            write(io, ':')
+            JSON.print(io, node.id)
+            write(io, ',')
+            JSON.print(io, "callFrame")
+            write(io, ':')
+            write(io, '{')
+                JSON.print(io, "functionName")
+                write(io, ':')
+                JSON.print(io, node.callFrame.functionName)
+                write(io, ',')
+                JSON.print(io, "scriptId")
+                write(io, ':')
+                JSON.print(io, node.callFrame.scriptId)
+                write(io, ',')
+                JSON.print(io, "url")
+                write(io, ':')
+                JSON.print(io, node.callFrame.url)
+                write(io, ',')
+                JSON.print(io, "lineNumber")
+                write(io, ':')
+                JSON.print(io, node.callFrame.lineNumber)
+                write(io, ',')
+                JSON.print(io, "columnNumber")
+                write(io, ':')
+                JSON.print(io, node.callFrame.columnNumber)
+            write(io, '}')
+            write(io, ',')
+            JSON.print(io, "hitCount")
+            write(io, ':')
+            JSON.print(io, node.hitCount)
+            write(io, ',')
+            JSON.print(io, "children")
+            write(io, ':')
+            JSON.print(io, node.children)
+            write(io, ',')
+            JSON.print(io, "positionTicks")
+            write(io, ':')
+            write(io, '[')
+            for (j, tick) in enumerate(node.positionTicks)
+                write(io, '{')
+                JSON.print(io, "line")
+                write(io, ':')
+                JSON.print(io, tick.line)
+                write(io, ',')
+                JSON.print(io, "ticks")
+                write(io, ':')
+                JSON.print(io, tick.ticks)
+                write(io, '}')
+                j == length(node.positionTicks) || write(io, ',')
+            end
+            write(io, ']')
+            write(io, '}')
+            i == length(profile.nodes) || write(io, ',')
+        end
+        write(io, ']')
+        write(io, ',')
+        JSON.print(io, "startTime")
+        write(io, ':')
+        JSON.print(io, profile.startTime)
+        write(io, ',')
+        JSON.print(io, "endTime")
+        write(io, ':')
+        JSON.print(io, profile.endTime)
+        write(io, ',')
+        JSON.print(io, "samples")
+        write(io, ':')
+        JSON.print(io, profile.samples)
+        write(io, ',')
+        JSON.print(io, "timeDeltas")
+        write(io, ':')
+        JSON.print(io, profile.timeDeltas)
+        write(io, '}')
+    end
+    nothing
+end
+
 end # module
+    
